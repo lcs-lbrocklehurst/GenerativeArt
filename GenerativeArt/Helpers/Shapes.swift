cda503477933c8146391f594ef954a7f1beb7a87
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-10-24 12:36:59 -0400

    Added the Flip option to the View

--- GenerativeArt/Helpers/Shapes.swift
+++ GenerativeArt/Helpers/Shapes.swift
@@ -6,3 +6,81 @@
 //
 
 import SwiftUI
+
+struct TriangleBottomLeft: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Define Path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        
+        // Return Path
+        return path
+        
+    }
+}
+
+struct TriangleTopLeft: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+            //Define Path
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        
+        // Return Path
+        return path
+    }
+}
+
+struct TriangleBottomRIght: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Define Path
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        
+        // Return the Path
+        return path
+    }
+}
+
+
+struct TriangleTopRight: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Define Path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY)) // 1
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY)) // 2
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) // 3
+        
+        // Return the path
+        return path
+
+    }
+}
--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -7,119 +7,64 @@
 
 import SwiftUI
 
-struct TriangleBottomLeft: Shape {
+enum Coin: Int {
+    case heads = 1
+    case tails = 2
     
-    func path(in rect: CGRect) -> Path {
+    static func flip() -> Coin {
+        let decision = Bool.random()
         
-        // Make an empty path
-        var path = Path()
-        
-        // Define Path
-        path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        
-        // Return Path
-        return path
-        
+        if decision == true {
+            return .heads
+        } else {
+            return .tails
+        }
     }
 }
 
-struct TriangleTopLeft: Shape {
-    
-    func path(in rect: CGRect) -> Path {
-        
-        var path = Path()
-        
-            //Define Path
-        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
-        
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        
-        // Return Path
-        return path
-    }
-}
 
-struct TriangleBottomRIght: Shape {
-    
-    func path(in rect: CGRect) -> Path {
-        
-        // Make an empty path
-        var path = Path()
-        
-        // Define Path
-        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
-        
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        
-        // Return the Path
-        return path
-    }
-}
 
-struct TriangleTopRight: Shape {
-    
-    func path(in rect: CGRect) -> Path {
-        
-        // Make an empty path
-        var path = Path()
-        
-        // Define Path
-        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY)) // 1
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY)) // 2
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) // 3
-        
-        // Return the path
-        return path
 
-    }
-}
 
 struct TileView: View {
+    
+    let flipOne = Coin.flip()
+    
     var body: some View {
         
-        VStack {
-            //Top righ Triangle
-           TriangleTopRight()
-                .stroke(.red)
-                .fill()
-            // Make the triangle fit the View modifier
-                .aspectRatio(1.0, contentMode: .fit)
+        ZStack {
             
-            // Bottom right triangle
+            if flipOne == .heads {
+                TriangleTopRight()
+                    .stroke(.red)
+                    .fill()
+                    .aspectRatio(1.0, contentMode: .fit)
+                
+                TriangleBottomLeft()
+                    .stroke(.red)
+                    .fill()
+                    .aspectRatio(1.0, contentMode: .fit)
+                                
+            } else {
+
+                
+                TriangleTopLeft()
+                    .stroke(.red)
+                    .fill()
+                    .aspectRatio(1.0, contentMode: .fit)
+                
+                TriangleBottomRIght()
+                    .stroke(.red)
+                    .fill()
+                    .aspectRatio(1.0, contentMode: .fit)
+            }
             
-            TriangleBottomRIght()
-                .stroke(.red)
-                .fill()
-            
-            //Make the Triangle Fit the View Modifier
-                .aspectRatio(1.0, contentMode: .fit)
-            
-            TriangleBottomLeft()
-                .stroke(.red)
-                .fill()
-           // Make the triangle fit the View modifier
-                .aspectRatio(1.0, contentMode: .fit)
-            
-            // Top Left Triangle
-            TriangleTopLeft()
-                .stroke(.red)
-                .fill()
-                .aspectRatio(1.0, contentMode: .fit)
-            //Bottom right Triangle
+           
+           
         }
         .padding()
     }
-    }
+}
 
 
 #Preview {
