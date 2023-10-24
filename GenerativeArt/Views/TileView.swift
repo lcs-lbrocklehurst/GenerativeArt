13f52b751bf9bc23b0f73100d080a48b2893feb0
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-10-24 11:55:48 -0400

    Drawing top-right Triangle.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -7,11 +7,32 @@
 
 import SwiftUI
 
+struct TriangleTopRight: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        // Make an empty path
+        var path = Path()
+        
+        // Define Path
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY)) // 1
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY)) // 2
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY)) // 3
+        
+        // Return the path
+        return path
+
+    }
+}
+
 struct TileView: View {
     var body: some View {
         
         VStack {
-           Rectangle()
+            //Top righ Triangle
+           TriangleTopRight()
                 .aspectRatio(1.0, contentMode: .fit)
         }
         .padding()
