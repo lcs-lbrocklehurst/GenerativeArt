c55a58896b2a325b17ffaa61b2b49e2816db2b92
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-10-24 11:44:58 -0400

    Organized Files and groups in the project

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  TileView.swift
 //  GenerativeArt
 //
 //  Created by Lewis Brocklehurst on 2023-10-24.
@@ -7,16 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TileView: View {
     var body: some View {
+        
         VStack {
            Rectangle()
                 .aspectRatio(1.0, contentMode: .fit)
         }
         .padding()
     }
-}
+    }
+
 
 #Preview {
-    ContentView()
+    TileView()
 }
