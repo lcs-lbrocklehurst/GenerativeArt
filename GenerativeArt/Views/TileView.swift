d17c8217e00fbddfa6b699ad0ff5563b41b56d25
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2023-10-26 14:46:11 -0400

    Added code to implement color selection according to the algorithm.

--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -30,19 +30,44 @@ struct TileView: View {
     
     let flipOne = Coin.flip()
     
+    // Choosing my markers
+    let markerOne = Color.red
+    let markerTwo = Color.blue
+    
+    // Decide on colors
+    let flipForColor = Coin.heads
+    
+    // MARK: Computed properties
+    
+    // Set colors based on the coin flip
+    var colorOne: Color {
+        if flipForColor == .heads {
+            return markerOne
+        } else {
+            return markerTwo
+        }
+    }
+    var colorTwo: Color {
+        if flipForColor == .heads {
+            return markerTwo
+        }else {
+            return markerOne
+        }
+    }
+    
     var body: some View {
         
         ZStack {
-            
+           
             if flipOne == .heads {
                 TriangleTopRight()
                     .stroke(.red)
-                    .fill()
+                    .fill(colorOne)
                     .aspectRatio(1.0, contentMode: .fit)
                 
                 TriangleBottomLeft()
                     .stroke(.red)
-                    .fill()
+                    .fill(colorTwo)
                     .aspectRatio(1.0, contentMode: .fit)
                                 
             } else {
